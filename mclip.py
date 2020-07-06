71b7bcd47f2c77042dfa8776d4ce7f95ce2da411
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-07 01:01:04 +0300

    short messages to convey quick information

--- mclip.py
+++ mclip.py
@@ -6,9 +6,9 @@
 # mclip.py - store and easily copy multiple messages to your computer's clipboard 
 
 TEXT = {
-    'reminder': '''Hey <customers' name>, quick reminder about your meeting tomorrow with <employee name> at <address>''',
-    'subscribe': '''Hi <name>, thanks for opting in for SMS updates from <your company>! You can text STOP at any time to unsubscribe''',
-    'status-alert': '''Hello <name>, this is <employee name> from <your company>. We are still working on your order, but we expect that it will be complete by <date>. Please text back if you have any questions!''',
+    'reminder': '''Hey, quick reminder about your meeting tomorrow with''',
+    'subscribe': '''Hi, thanks for opting in for SMS updates. You can text STOP at any time to unsubscribe''',
+    'status-alert': '''Hello, we are still working on your order''',
     'agree': '''Yes, I agree. That sounds fine to me''',
     'busy': '''Sorry. Can we do this later this week or next week kindly.'''
 }
